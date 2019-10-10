fc8b499269b1122a57df347db9b2c43e28407a61
Lasse Claes Jacobsen <lacj@Lasses-MacBook-Pro.local>
2019-10-10 21:13:13 +0200

    Implement instagram feed updated

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,31 @@
 $(function () { // DOM ready for jQuery??
   //write your code below
 
+  //alert("helo")
+  $.instagramFeed({
+    'username': 'sb1864',
+    /*'tag': 'sb1864',*/
+    'container': "#my-instagram-feed",
+    'image_size': [150, 240, 320, 480, 640],
+    'display_profile': true,
+    'display_biography': true,
+    'display_gallery': true,
+    /*'get_data': false,
+    'callback': "seeJson",*/
+    /*'styling': true, */
+    'items': 100,
+    'items_per_row': 4,
+    'margin': 1
+    /*'username': 'lasseclaes',
+    'container': "#my-instagram-feed",
+    'get_data': true,
+    'callback': seeJson*/
+  });
+
+  /*function seeJson(data) {
+    console.log(data);
+  }*/
+
   //The two go to debugging tools: alert & console.log - uncomment them using // or /* .... */
   //alert("hey");
   console.log('hi there from "js/scripts.js" - good to see you');
