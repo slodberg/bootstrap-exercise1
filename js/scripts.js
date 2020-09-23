5312b10c59f34b9d9bb03991332164a7a5f9f01c
Lasse Claes Jacobsen <lacj@Lasses-MacBook-Pro.local>
2020-09-23 22:23:43 +0200

    solution v5 (& v.4)  but unsolved issues with  ".row .row-cols-xxx ~ .row .row-cols-xxx" being nested instead of making a new row

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,6 @@
 
   //The two go to debugging tools: alert & console.log - uncomment them using // or /* .... */
   //alert("hey");
-  alert("hey");
+  //alert("hey");
   console.log('hi there from "js/scripts.js" - good to see you');
   console.error('Just checking if YOU are checking the console. You should always open Chrome DevTools and check the console when doing web development. \n\nPlease uncomment or delete these messages from the js-file. \n\nBUT remember the syntax for alert and console.log \(e.g. by keeping then in the comments in the file\). \n\n//Lasse');
