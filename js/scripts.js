dcbd6035aa21a626b1892788e1f5c1b8bdd0b835
Lasse Claes Jacobsen <lacj@Lasses-MacBook-Pro.local>
2020-09-27 22:50:11 +0200

    minor changes

--- js/scripts.js
+++ js/scripts.js
@@ -4,4 +4,4 @@
   //alert("hey");
   //alert("hey");
   console.log('hi there from "js/scripts.js" - good to see you');
-  console.error('Just checking if YOU are checking the console. You should always open Chrome DevTools and check the console when doing web development. \n\nPlease uncomment or delete these messages from the js-file. \n\nBUT remember the syntax for alert and console.log \(e.g. by keeping then in the comments in the file\). \n\n//Lasse');
+  //console.error('Just checking if YOU are checking the console. You should always open Chrome DevTools and check the console when doing web development. \n\nPlease uncomment or delete these messages from the js-file. \n\nBUT remember the syntax for alert and console.log \(e.g. by keeping then in the comments in the file\). \n\n//Lasse');
